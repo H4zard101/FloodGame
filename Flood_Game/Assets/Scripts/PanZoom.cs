e8c13d38424c7cc0f45f179657ede981dede2496
H4zard101 <Harry2.Hodgkins@live.uwe.ac.uk>
2022-12-12 18:42:20 +0000

    fixed the zoom in stuff

--- Flood_Game/Assets/Scripts/PanZoom.cs
+++ Flood_Game/Assets/Scripts/PanZoom.cs
@@ -6,42 +6,47 @@ public class PanZoom : MonoBehaviour
 {
     Vector3 touchStart;
     public float zoomOutMin = 1;
-    public float zoomOutMax = 8;
+    public float zoomOutMax = 120;
     public float cameraSpeed = 0;
+    public Camera playerCamera;
 
     // Update is called once per frame
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            touchStart = playerCamera.ScreenToWorldPoint(Input.mousePosition);
         }
-        if (Input.touchCount == 2)
-        {
-            Touch touchZero = Input.GetTouch(0);
-            Touch touchOne = Input.GetTouch(1);
+        //if (Input.touchCount == 2)
+        //{
+        //    Touch touchZero = Input.GetTouch(0);
+        //    Touch touchOne = Input.GetTouch(1);
 
-            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+        //    Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+        //    Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
 
-            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+        //    float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+        //    float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
 
-            float difference = currentMagnitude - prevMagnitude * cameraSpeed * Time.deltaTime;
-            
+        //    float difference = currentMagnitude - prevMagnitude * cameraSpeed * Time.deltaTime;
 
-            zoom(difference * 0.01f);
-        }
+
+        //    zoom(difference * 0.01f);
+        //}
         else if (Input.GetMouseButton(0))
         {
-            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Camera.main.transform.position += direction;
+            Vector3 direction = touchStart - playerCamera.ScreenToWorldPoint(Input.mousePosition);
+            playerCamera.transform.position += direction;
         }
-        zoom(Input.GetAxis("Mouse ScrollWheel"));
+        //zoom(Input.GetAxis("Mouse ScrollWheel"));
+
+        playerCamera.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * cameraSpeed;
+        playerCamera.orthographicSize = Mathf.Clamp(playerCamera.orthographicSize - Input.GetAxis("Mouse ScrollWheel"), zoomOutMin, zoomOutMax);
     }
 
-    void zoom(float increment)
-    {
-        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
-    }
+    //void zoom(float increment)
+    //{
+    //    Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
+    //}
 }
