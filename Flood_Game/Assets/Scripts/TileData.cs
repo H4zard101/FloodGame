3e03796936d47bb501e7a8a857052579a821f8e1
H4zard101 <Harry2.Hodgkins@live.uwe.ac.uk>
2022-12-12 18:16:13 +0000

    added a script called TileData

---
+++ Flood_Game/Assets/Scripts/TileData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileData : MonoBehaviour
+{
+    public float currentTileLevel = 0.5F;
+    public float maximumTileLevel = 1.0F;
+    public bool isOverFlooded = false;
+}
