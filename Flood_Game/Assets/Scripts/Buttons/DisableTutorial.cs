c0fd97e4530b5e8f2bfd8a953f47d03094366ca1
H4zard101 <Harry2.Hodgkins@live.uwe.ac.uk>
2023-02-01 20:09:29 +0000

    added some Tutorial UI, not fully synced yet but pretty much everything is there

--- Flood_Game/Assets/Scripts/Buttons/DisableTutorial.cs
+++ Flood_Game/Assets/Scripts/Buttons/DisableTutorial.cs
@@ -5,8 +5,23 @@ using UnityEngine;
 public class DisableTutorial : MonoBehaviour
 {
     public GameObject TutorialStuff;
+    public GameObject BuildUI;
+    public GameObject MapOverView;
     public void disableTut()
     {
         TutorialStuff.SetActive(false);
+        BuildUI.SetActive(true);
+        MapOverView.SetActive(false);
+    }
+
+    public void enableTut()
+    {
+        TutorialStuff.SetActive(false);
+        MapOverView.SetActive(true);
+    }    
+
+    public void MapOverViewUI()
+    {
+
     }
 }
