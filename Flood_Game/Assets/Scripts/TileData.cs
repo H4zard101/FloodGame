cdf587de343df31affdb51457b74ed078d99d92c
H4zard101 <Harry2.Hodgkins@live.uwe.ac.uk>
2023-02-21 23:07:36 +0000

    Begining to work on the mesh filters being swapped out depending on the current water level of each tile

--- Flood_Game/Assets/Scripts/TileData.cs
+++ Flood_Game/Assets/Scripts/TileData.cs
@@ -4,7 +4,27 @@ using UnityEngine;
 
 public class TileData : MonoBehaviour
 {
-    public float currentTileLevel = 0.5F;
+    public float currentTileLevel = 0.1F;
     public float maximumTileLevel = 1.0F;
     public bool isOverFlooded = false;
+
+    public MeshFilter objectMesh;
+    public void Update()
+    {
+        // WATER BLOCK
+        if(currentTileLevel >= 0.9f)
+        {
+            
+        }
+        // MUD BLOCK
+        if (currentTileLevel >= 0.7f)
+        {
+
+        }
+        // GRASS BLOCK
+        if (currentTileLevel <= 0.5f)
+        {
+
+        }
+    }
 }
