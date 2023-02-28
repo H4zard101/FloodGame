aed318d363a29ff9937920d45583237ed0b09608
H4zard101 <Harry2.Hodgkins@live.uwe.ac.uk>
2023-02-28 04:03:23 +0000

    Added Ui for when the game is paused, also changed the tile blocks to be the new version so that they all use the same materia. The filters now change depending on the water level of that tile

--- Flood_Game/Assets/Scripts/TileData.cs
+++ Flood_Game/Assets/Scripts/TileData.cs
@@ -8,23 +8,28 @@ public class TileData : MonoBehaviour
     public float maximumTileLevel = 1.0F;
     public bool isOverFlooded = false;
 
-    public MeshFilter objectMesh;
+    public MeshFilter waterMeshFilter;
+    public MeshFilter grassMeshFilter;
+    public MeshFilter mudMeshFilter;
     public void Update()
     {
-        // WATER BLOCK
-        if(currentTileLevel >= 0.9f)
-        {
-            
-        }
-        // MUD BLOCK
-        if (currentTileLevel >= 0.7f)
-        {
 
-        }
         // GRASS BLOCK
         if (currentTileLevel <= 0.5f)
         {
+            this.gameObject.GetComponent<MeshFilter>().sharedMesh = grassMeshFilter.sharedMesh;
+        }
+        // WATER BLOCK
+        else if (currentTileLevel >= 0.9f)
+        {
+            this.gameObject.GetComponent<MeshFilter>().sharedMesh = waterMeshFilter.sharedMesh;
 
+
+        }
+        // MUD BLOCK
+        else if (currentTileLevel >= 0.7f)
+        {
+            this.gameObject.GetComponent<MeshFilter>().sharedMesh = mudMeshFilter.sharedMesh;
         }
     }
 }
