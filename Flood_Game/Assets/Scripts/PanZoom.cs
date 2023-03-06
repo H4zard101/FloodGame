8c735ba3b0f70bada56603fc351c03cb13759258
MariamKhakoo <48660043+MariamKhakoo@users.noreply.github.com>
2023-03-06 01:02:04 +0000

    cloud spawning. raindrop model created and implemented

--- Flood_Game/Assets/Scripts/PanZoom.cs
+++ Flood_Game/Assets/Scripts/PanZoom.cs
@@ -50,7 +50,7 @@ public class PanZoom : MonoBehaviour
     //    Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
     //}
 }*/
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -90,4 +90,49 @@ public class PanZoom : MonoBehaviour
             playerCamera.transform.RotateAround(playerCamera.transform.position, playerCamera.transform.right, -rotationY);
         }
     }
+}*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanZoom : MonoBehaviour
+{
+    Vector3 touchStart;
+    public float zoomOutMin = 1;
+    public float zoomOutMax = 120;
+    public float cameraSpeed = 0;
+    public float rotateSpeed = 5;
+    public Camera playerCamera;
+
+    private Quaternion targetRotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart = playerCamera.ScreenToWorldPoint(Input.mousePosition);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = touchStart - playerCamera.ScreenToWorldPoint(Input.mousePosition);
+            playerCamera.transform.position += direction;
+        }
+
+        // Zoom in/out
+        playerCamera.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * cameraSpeed;
+        playerCamera.orthographicSize = Mathf.Clamp(playerCamera.orthographicSize, zoomOutMin, zoomOutMax);
+
+        // Rotate camera
+        if (Input.GetMouseButton(1))
+        {
+            float rotationX = Input.GetAxis("Mouse X") * rotateSpeed;
+            float rotationY = Input.GetAxis("Mouse Y") * rotateSpeed;
+
+            targetRotation = Quaternion.Euler(playerCamera.transform.rotation.eulerAngles + new Vector3(-rotationY, rotationX, 0));
+        }
+
+        playerCamera.transform.rotation = Quaternion.Lerp(playerCamera.transform.rotation, targetRotation, Time.deltaTime * 5f);
+    }
 }
+
