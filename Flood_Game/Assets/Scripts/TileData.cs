75a22f0d27195072af29a48d0b582501e6836ceb
H4zard101 <Harry2.Hodgkins@live.uwe.ac.uk>
2023-03-14 03:29:11 +0000

    Added some stuff

--- Flood_Game/Assets/Scripts/TileData.cs
+++ Flood_Game/Assets/Scripts/TileData.cs
@@ -1,35 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TileData : MonoBehaviour
 {
+    // water levels
     public float currentTileLevel = 0.1F;
     public float maximumTileLevel = 1.0F;
-    public bool isOverFlooded = false;
+    public float amountExeding = 0.0f;
 
+    // will be the neighbour tiles
+    public List<GameObject> neighbours = new List<GameObject>();
+
+    // Diffrent MeshTypes
     public MeshFilter waterMeshFilter;
     public MeshFilter grassMeshFilter;
-    public MeshFilter mudMeshFilter;
+    public MeshFilter grassWithWater1;
+    public MeshFilter grassWithWater2;
+    public MeshFilter grassWithWater3;
+    //public MeshFilter mudMeshFilter;
+
+    public void Start()
+    {
+        //AddNeighbours();
+    }
     public void Update()
     {
+        UpdateTileMesh();
 
+        if (currentTileLevel >= 1.0f)
+        {
+            AffectNeighbours();
+        }
+
+    }
+    public void UpdateTileMesh()
+    {
         // GRASS BLOCK
-        if (currentTileLevel <= 0.5f)
+        if (currentTileLevel <= 0.2f)
         {
             this.gameObject.GetComponent<MeshFilter>().sharedMesh = grassMeshFilter.sharedMesh;
+
+        }
+        // GRASS BLOCK WITH SOME WATER
+        else if (currentTileLevel <= 0.4f)
+        {
+            this.gameObject.GetComponent<MeshFilter>().sharedMesh = grassWithWater1.sharedMesh;
+
+        }
+        // GRASS BLOCK WITH SOME WATER 2
+        else if (currentTileLevel <= 0.6f)
+        {
+            this.gameObject.GetComponent<MeshFilter>().sharedMesh = grassWithWater2.sharedMesh;
+
+        }
+        // GRASS BLOCK WITH SOME WATER 2
+        else if (currentTileLevel <= 0.8f)
+        {
+            this.gameObject.GetComponent<MeshFilter>().sharedMesh = grassWithWater3.sharedMesh;
+
         }
         // WATER BLOCK
-        else if (currentTileLevel >= 0.9f)
+        else if (currentTileLevel >= 1.0f)
         {
             this.gameObject.GetComponent<MeshFilter>().sharedMesh = waterMeshFilter.sharedMesh;
 
+        }
+    }
 
-        }
-        // MUD BLOCK
-        else if (currentTileLevel >= 0.7f)
+    public void AffectNeighbours()
+    {
+        amountExeding = currentTileLevel - maximumTileLevel;
+        for (int i = 0; i < neighbours.Count; i++)
         {
-            this.gameObject.GetComponent<MeshFilter>().sharedMesh = mudMeshFilter.sharedMesh;
+            neighbours[i].GetComponent<TileData>().currentTileLevel += amountExeding;
         }
+        currentTileLevel -= amountExeding;
     }
 }
